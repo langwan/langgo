354d69a89b9f1fe7ed99770cd5630a12619d237d
langwanluo <langwnaluo@gmail.com>
2022-12-09 14:07:39 +0800

    remove examples

--- examples/log/cut.sh
+++
@@ -1,3 +0,0 @@
-echo "rotating..."
-mv ./logs/app.log ./logs/app.log-old
-killall -USR2 main
--- examples/mysql/account.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "gorm.io/gorm"
-
-type Account struct {
-	gorm.Model
-	Name string
-}

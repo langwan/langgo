1ca955fc6b6addfc5acf3708b9c60c91b1bab7ad
langwanluo <langwnaluo@gmail.com>
2023-02-24 16:09:21 +0800

    update helper_call

--- helpers/code/code.go
+++ helpers/code/code.go
@@ -2,13 +2,15 @@ package helper_code
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	jsoniter "github.com/json-iterator/go"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"reflect"
 )
 
-func Call(ctx context.Context, service interface{}, methodName string, request string) (response interface{}, code int, err error) {
+func CallGrpc(ctx context.Context, service interface{}, methodName string, request string) (response interface{}, code int, err error) {
 	tp := reflect.TypeOf(service)
 	method, ok := tp.MethodByName(methodName)
 	if !ok {
@@ -32,4 +34,28 @@ func Call(ctx context.Context, service interface{}, methodName string, request s
 	}
 
 	return call[0].Interface(), 0, nil
+}
+
+func Call(ctx context.Context, service interface{}, methodName string, request string) (response interface{}, err error) {
+	tp := reflect.TypeOf(service)
+	method, ok := tp.MethodByName(methodName)
+	if !ok {
+		return "", errors.New(fmt.Sprintf("%s not find", methodName))
+	}
+
+	method.Type.NumIn()
+
+	parameter := method.Type.In(2)
+	req := reflect.New(parameter.Elem()).Interface()
+	jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal([]byte(request), req)
+
+	in := make([]reflect.Value, 0)
+	in = append(in, reflect.ValueOf(ctx))
+	in = append(in, reflect.ValueOf(req))
+	call := reflect.ValueOf(service).MethodByName(methodName).Call(in)
+	if call[1].Interface() != nil {
+		e := call[1].Interface().(error)
+		return "", e
+	}
+	return call[0].Interface(), nil
 }

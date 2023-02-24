3125b637baf36a503873688f8bbdff59de9310de
langwanluo <langwnaluo@gmail.com>
2023-02-24 16:10:53 +0800

    helper_string add function

--- helpers/string/string.go
+++ helpers/string/string.go
@@ -18,3 +18,42 @@ func Utf8StringLength(str string) int {
 func Utf8TruncateText(text string, max int, omission string) string {
 	return truncate.Truncate(text, max, omission, truncate.PositionEnd)
 }
+
+func SetSuffix(s string, suffix string) string {
+	if strings.HasSuffix(s, suffix) {
+		return s
+	} else {
+		var builder strings.Builder
+		builder.Grow(len(s) + len(suffix))
+		builder.WriteString(s)
+		builder.WriteString(suffix)
+		return builder.String()
+	}
+}
+func RemoveSuffix(s string, suffix string) string {
+	if !strings.HasSuffix(s, suffix) {
+		return s
+	} else {
+		return s[:len(s)-len(suffix)]
+	}
+}
+
+func SetPrefix(s string, prefix string) string {
+	if strings.HasPrefix(s, prefix) {
+		return s
+	} else {
+		var builder strings.Builder
+		builder.Grow(len(s) + len(prefix))
+		builder.WriteString(prefix)
+		builder.WriteString(s)
+		return builder.String()
+	}
+}
+
+func RemovePrefix(s string, prefix string) string {
+	if !strings.HasPrefix(s, prefix) {
+		return s
+	} else {
+		return s[len(prefix):]
+	}
+}
